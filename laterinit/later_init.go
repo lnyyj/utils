f00d9f2425869d8236eafa561c839a87a73be001
songshenyang <shenyang.song269@imaygou.com>
2022-01-07 12:02:13 +0800

    update

---
+++ laterinit/later_init.go
@@ -0,0 +1,35 @@
+package laterinit
+
+import (
+	"log"
+)
+
+type initFunc func() error
+
+type initModule struct {
+	name string
+	fn   initFunc
+}
+
+var initFuncs []*initModule
+
+func init() {
+	initFuncs = make([]*initModule, 0)
+}
+
+func RegisterInit(name string, fn initFunc) {
+	log.Println("register module ", name)
+	initFuncs = append(initFuncs, &initModule{
+		name: name,
+		fn:   fn,
+	})
+}
+
+func Init() {
+	for _, service := range initFuncs {
+		log.Println("init module  ", service.name)
+		if err := service.fn(); err != nil {
+			panic(err)
+		}
+	}
+}
